a0d7c465731d1cec0c9dfeecaa3f9f1241fd9358
Vadim Korol <55877443+korolvv@users.noreply.github.com>
2024-05-08 14:16:41 +0300

    add class name for button

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,9 +3,15 @@ import css from "./Options.module.css";
 export default function Options({ onUpdate, total, onReset }) {
 	return (
 		<>
-			<button onClick={() => onUpdate("good")}>Good</button>
-			<button onClick={() => onUpdate("neutral")}>Neutral</button>
-			<button onClick={() => onUpdate("bad")}>Bad</button>
+			<button className={css.btn} onClick={() => onUpdate("good")}>
+				Good
+			</button>
+			<button className={css.btn} onClick={() => onUpdate("neutral")}>
+				Neutral
+			</button>
+			<button className={css.btn} onClick={() => onUpdate("bad")}>
+				Bad
+			</button>
 			{total !== 0 && <button onClick={onReset}>Reset</button>}
 		</>
 	);
